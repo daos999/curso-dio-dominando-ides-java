619d7f123d6bb12f5342578424e4202e17bc12ea
Daos999 <daosptc@gmail.com>
2021-09-23 09:29:49 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+		
+		
+		
+		
+		/*int a = 1, b = 5;
+		System.out.println("Hello Word " + (a+b));
+		*/
+	}
+
+}
